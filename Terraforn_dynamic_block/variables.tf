afa049b52f81f660e5857dcdddaf7414cbe92f8d
Pr4Mukh <62900685+Sanchit1756@users.noreply.github.com>
2024-12-02 21:59:41 +0530

    Added map variable in variable block , need to test block

--- Terraforn_dynamic_block/variables.tf
+++ Terraforn_dynamic_block/variables.tf
@@ -8,3 +8,15 @@ variable "sg_ports" {
     default = [ 22, 443, 80, 3389 , 0]
 }
 
+variable "sg_ports_map" {
+    type = map(object({
+        from_port = number,
+        to_port = number,
+        protocol = string,
+        cidr_block = list(string)
+    }))
+    default = [
+        from_port = 443 ,to_port = 80.value ,protocol = "tcp" ,cidr_block = ["0.0.0.0/0"],
+        from_port = 8080 ,to_port = 224.value ,protocol = "tcp" ,cidr_block = ["0.0.0.0/0"],
+    ]
+}
